44d27bd23911bd67125295c9a8052d68aa346f9e
walkline <walkline.ua@gmail.com>
2015-07-17 12:16:10 +0300

    Initial Commit

---
+++ MOCPulse OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MOCPulse OSX
+//
+//  Created by admin on 17.07.15.
+//  Copyright (c) 2015 Master of Code. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MOCPulse OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MOCPulse OSX
+//
+//  Created by admin on 17.07.15.
+//  Copyright (c) 2015 Master of Code. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
